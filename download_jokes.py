5857faa0ba1b0e840e7ca4bcec50373798aa287f
Dervun <masharskyss@gmail.com>
2017-12-11 20:59:50 +0300

    use of anonymizer, some bugs fixed (my ip was bloked on the site, anonymizer gives higher speed than proxy)

--- download_jokes.py
+++ download_jokes.py
@@ -10,12 +10,12 @@ proxy_list = ['http://122.183.139.105:8080',
               'http://180.248.3.82:8080'
 ]
 
-start = 468030
-stop = 400000
+start = 100000
+stop = 0
 k = 2
 with open('jokes.txt', 'a', encoding='utf-8') as output_file:
     for n in range(start, stop, -1):
-        url = 'http://www.anekdot.ru/id/{0:d}/'.format(n)
+        url = 'http://0s.o53xo.mfxgk23en52c44tv.cmle.ru/id/{0:d}/'.format(n)
         headers = {
             'User-Agent':
                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
@@ -41,11 +41,25 @@ with open('jokes.txt', 'a', encoding='utf-8') as output_file:
             continue
 
         if r.status_code != requests.codes.ok:
-            print(r.status_code, 'sleep for 2 seconds')
+            print(r.status_code, 'sleep for 1 seconds')
             k += 1
-            time.sleep(2)
+            time.sleep(1)
             print('try to get request')
-            r = requests.get(url, headers=headers)
+            try:
+                if use_proxy:
+                    r = requests.get(url, headers=headers, proxies=proxies)
+                else:
+                    r = requests.get(url, headers=headers)
+            except TimeoutError:
+                print('TimeoutError')
+                k += 1
+                time.sleep(10)
+                continue
+            except requests.exceptions.ConnectionError:
+                print('requests.exceptions.ConnectionError')
+                k += 1
+                time.sleep(10)
+                continue
 
         current_page = html.fromstring(r.text)
 
@@ -56,8 +70,8 @@ with open('jokes.txt', 'a', encoding='utf-8') as output_file:
             continue
         text_node = div_node.xpath('./div[@class="text"]')[0]
 
-        # add only text jokes
-        if len(text_node.xpath('.//img')) == 0:
+        # add only text jokes, without links
+        if len(text_node.xpath('.//img')) == 0 and len(text_node.xpath('.//a')) == 0:
             rates_node = div_node.xpath('./div[@class="rates"]')[0]
             text_of_number = 'number=' + str(n) + '\n'
             text_of_joke = '\n'.join(text_node.xpath('./text()'))
